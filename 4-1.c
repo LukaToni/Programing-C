b7461e68715c82b3704c21e56469f10ba216aaa9
LukaToni <luka.toni93@gmail.com>
2014-06-20 14:10:34 +0200

    Update 4-1.c

--- 4-1.c
+++ 4-1.c
@@ -2,7 +2,46 @@
 
 int main()
 {
-
-
-  retrun 0;
+  int days = 1;
+  int day = 0;
+  int Tday = 0;
+  int length = 0;
+  int TLength  = 0;
+  
+  int temp;
+  
+  while(scanf("%d", temp) == 1 && temp != EOF)
+  {
+    if(temp >= 300 && Tday == 0 && TLength == 0)
+    {
+      Tday = days;
+      TLength++;
+      days++;  
+      continue;
+    }
+    
+    if(temp >= 300 && Tday != 0)
+    {
+      TLength++;
+      days++;
+      continue;
+    }
+    
+    if(temp < 300)
+    {
+      if (length < TLength)
+      {
+        day = Tday;
+        length = TLength;
+      }
+      
+      if(temp == -9999)
+        break;
+    }
+    
+    Tday = 0;
+    TLength = 0;
+    days++;  
+  }
+  return 0;
 }
